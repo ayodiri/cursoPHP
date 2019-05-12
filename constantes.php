4c0a97f5f8db13dd6c6ce83b9e00a6e908c95390
AitziRaku <ayodiri@gmail.com>
2019-05-12 17:45:26 +0200

    constantes

---
+++ constantes.php
@@ -0,0 +1,10 @@
+<?php 
+    /**
+     * nos permite establecer un valor a una variable pero no se
+     * puede modificar
+     */
+    define('PI', 3.141516);
+    define('NOMBRE', 'Aitziber');
+    echo PI;
+    echo NOMBRE;
+?>
