d85c5df3a93f84de23e5fd7d687023a7cf5ca7a2
AitziRaku <ayodiri@gmail.com>
2019-05-12 18:20:06 +0200

    asociativos

--- arreglos.php
+++ arreglos.php
@@ -1,4 +1,8 @@
 <?php 
+    /**
+     * Arrays indexado
+     */
+    echo 'ARRAYS INDEXADOS' . '<br>';
     $semana = array('lunes', 'martes', 'miercoles', 'jueves', 'viernes', 'sabado', 'domingo');
     echo $semana[1] . '<br>';
     $semana[7] = 'FalconMasters';
@@ -6,5 +10,11 @@
     $semana2 = [ 'lunes', 'martes', 'miercoles', 'jueves', 'viernes', 'sabado', 'domingo'];
     echo $semana2[4] . '<br>';
 
-
+    /**
+     * Arrays asociativos
+     */
+    echo '<br>' . 'ARRAYS ASOCIATIVOS' . '<br>';
+    $alex = ['telefono' => '945264403', 'edad' => 36, 'apellido' => 'Garcia', 'pais' => 'Euskadi'];
+    echo 'Pais : ' . $alex['pais'] . '<br>';
+    echo 'Edad : ' . $alex['edad'] . '<br>';
 ?>
