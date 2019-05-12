0b043a1f86ad7b004fec637ee4d08bcd4a10d721
AitziRaku <ayodiri@gmail.com>
2019-05-12 17:25:35 +0200

    hola

---
+++ hola_mundo.php
@@ -0,0 +1,25 @@
+<?php 
+    //echo nos sirve para mostrar información en pantalla
+    # otro comentario
+    /* Otro
+    * comentario
+    * mas
+     */
+    echo "Hola Mundo";
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Page Title</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
+    <script src='main.js'></script>
+</head>
+<body>
+    <h1><?php echo "Hola Mundo soy un H1"; ?></h1>
+</body>
+</html>
