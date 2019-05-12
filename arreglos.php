7b00605cd8271c7aeb71ea728dccba2688aced73
AitziRaku <ayodiri@gmail.com>
2019-05-12 18:12:24 +0200

    arrays

---
+++ arreglos.php
@@ -0,0 +1,10 @@
+<?php 
+    $semana = array('lunes', 'martes', 'miercoles', 'jueves', 'viernes', 'sabado', 'domingo');
+    echo $semana[1] . '<br>';
+    $semana[7] = 'FalconMasters';
+    echo $semana[7] . '<br>';
+    $semana2 = [ 'lunes', 'martes', 'miercoles', 'jueves', 'viernes', 'sabado', 'domingo'];
+    echo $semana2[4] . '<br>';
+
+
+?>
